7163ba8aa7e4dcb756cb91165833f9e9b62d13ce
briseida camacho <evabri2008cue@gmail.com>
2025-10-13 11:58:08 -0300

    esta disponible la funcion extraer metadatos de noticas

---
+++ main.py
@@ -0,0 +1,7 @@
+from app.noticias import extraer_metadatos
+def main():
+    noticia=extraer_metadatos("https://www.lanacion.com.ar/economia/dolar/dolar-caen-las-cotizaciones-oficiales-tras-los-anuncios-de-bessent-nid13102025/")
+    print(noticia)
+
+if __name__=="__main__":
+    main()
